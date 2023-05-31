ef0f422bfdeb94f28a6ec4f35f3a34479f1cf301
慧大 <117243543+rinnjinn@users.noreply.github.com>
2023-05-31 11:44:39 +0900

    Add files via upload

---
+++ 福ちゃんねる/04_ソースコード/sakujyokanryou.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="css/loguinkanryou.css">
+
+<form name="loguinkanryou_form">
+<img src="MicrosoftTeams-image (19).png";>
+<div class="roguinkanryou_rogo">
+    <h1>スレッドを削除しました。</h1>
+    <h2>トップに戻る</h2>
+
+</div>   
+</form>
