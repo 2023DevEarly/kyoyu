92bf656a938f8a98b1fbe60d3c1f238b7c984d50
慧大 <117243543+rinnjinn@users.noreply.github.com>
2023-05-31 11:38:07 +0900

    Add files via upload

---
+++ 福ちゃんねる/04_ソースコード/loguinkanryou.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="css/loguinkanryou.css">
+<form name="loguinkanryou_form">
+<img src="MicrosoftTeams-image (19).png">
+<div class="roguinkanryou_rogo">
+    <h1>ログイン成功しました。</h1>
+    <h2>次へ</h2>
+
+</div>   
+</form>
