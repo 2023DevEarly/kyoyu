45194b675132d13d1f88f7b2828ea237a688a460
福山拓功 <117243226+FHiraku@users.noreply.github.com>
2023-07-12 10:51:34 +0900

    Update ログイン後スレッド.php

--- 福ちゃんねる/04_ソースコード/ログイン後スレッド.php
+++ 福ちゃんねる/04_ソースコード/ログイン後スレッド.php
@@ -26,38 +26,24 @@
 
     <div id="chatbox">
         <!-- チャットログが表示される領域 -->
-        <?php
-        $pdo = new PDO('mysql:host=localhost;dbname=chanel;charset=utf8','root','');
-        ?>
-        <?php
-// チャットメッセージを保存するためのファイル
-$chatFile = "chat_log.txt";
-
-// メッセージの送信者と内容を取得
-if (isset($_POST['sender']) && isset($_POST['message'])) {
-    $sender = $_POST['sender'];
-    $message = $_POST['message'];
-
-    // メッセージをファイルに追記
-    $log = "[" . date('Y-m-d H:i:s') . "] " . $sender . ": " . $message . "\n";
-    file_put_contents($chatFile, $log, FILE_APPEND | LOCK_EX);
-}
-
-// チャットログを取得して表示
-if (file_exists($chatFile)) {
-    $chatLog = file_get_contents($chatFile);
-    echo nl2br($chatLog); // 改行を<br>タグに変換して表示
-}
+    <?php   
+       $pdo = new PDO('mysql:host=localhost;dbname=chanel;charset=utf8','root','');
+       $sql="SELECT thread_id,title,comment FROM スレッド INNER JOIN メッセージ ON スレッド.thread_id = message.thread_id WHERE スレッド.thread_id= ?";
+       $ps=$pdo->prepare($sql);
+       $ps->bindValue(1,$_POST['thread'], PDO::PARAM_INT);
+       $ps->execute();
+       foreach($ps->fetchAll() as $row){
+       $title = $row['title'];
+       $comment = $row['comment'];
+       }
 ?>
 
-
-    </div>
-    <div class=box>
-    <form method="post" action="">
-        <input type="text" name="message" placeholder="メッセージ" required>
-        <input type ="image" name="submit" width="20" height="20" src="img/sousin.jpg">
+    <form method="post" action="ログイン後スレッドngworderror.php">
+        <label for="input">入力:</label>
+        <input type="text" id="input" name="input" required>
+        <br>
+        <input type="submit" value="送信">
     </form>
-</div>
-<a href='http://localhost/chanel/logtop.php'>◁</a>
+    <button type="button" class="btn btn-outline-dark" onclick="history.back(-1)">◁</button>
 </body>
 </html>
