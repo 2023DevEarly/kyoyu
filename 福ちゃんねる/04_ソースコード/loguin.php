3fb34d9859b51dfd51ac415fab9c7b755dec955a
Fmasato <117242987+Aso2101200@users.noreply.github.com>
2023-07-13 10:01:23 +0900

    Add files via upload

--- 福ちゃんねる/04_ソースコード/loguin.php
+++ 福ちゃんねる/04_ソースコード/loguin.php
@@ -2,21 +2,24 @@
 session_start();
 if(isset($_POST['user_name'])){
     $pdo = new PDO('mysql:host=localhost;dbname=kaihatu;charset=utf8','root','root');
-    $sql="SELECT * FROM ユーザー WHERE user_pass=?";
+    $sql="SELECT * FROM ユーザー WHERE user_mail=?";
     $ps=$pdo->prepare($sql);
-    $ps->bindValue(1,$_POST['password'],PDO::PARAM_STR);
+    $ps->bindValue($_POST['password'],PDO::PARAM_STR);
     $ps->execute();
     $Array = $ps->fetchAll();
 }
 
+$erro=[];
+
 if (empty($user_mail)) {
-  $_SESSION['error'] = 'メールアドレスが入力されていません。';
+  $_SESSION['error1'] = 'メールアドレスが入力されていません。';
+  $erro[] = $_SESSION['error1'];
 }
 
 if (empty($user_pass)) {
-  $_SESSION['error'] = 'パスワードが入力されていません。';
+  $_SESSION['error2'] = 'パスワードが入力されていません。';
+  $erro[] = $_SESSION['error2'];
 }
-
 
 ?>
 
@@ -39,35 +42,22 @@ if (empty($user_pass)) {
           メールアドレスとパスワードを入力して「ログイン」ボタンを押してください<br>
 
          メールアドレス　 <input type="email" name="user_mail" required><br>
+         <?php   
+                  if((isset($_SESSION['error1']))){
+                        echo '<div style="color: red; text-align: center;">'.$_SESSION['error1'].'</div>'.'<br>';
+                        unset($_SESSION['error1']);
+                  } ?>
          パスワード　　　    <input type="password" name="user_pass" required><br>
-          <?php   
-                  if((isset($_SESSION['error']))){
-                        echo '<div style="color: red; text-align: center;">'.$_SESSION['error'].'</div>'.'<br>';
-                        unset($_SESSION['error']);
-} ?><br>
+         <?php   
+                  if((isset($_SESSION['error2']))){
+                        echo '<div style="color: red; text-align: center;">'.$_SESSION['error2'].'</div>'.'<br>';
+                        unset($_SESSION['error2']);
+                  } ?>
+          <br>
        </div>
           
        <button type="button" class="btn1" 
        <?php 
         
         ?>>ログイン</button><br>
-</form>
-<form name="login_form2" action="loguinkanryou.php" method="POST" >
-      <div class="login_sin_">
-        <h1>新規登録</h1>
-      </div>
-
-      <div class="login_sinki_btm">
-      メールアドレスとパスワードを入力して「新規登録」ボタンを押してください<br>
-
-         ハンドルネーム　<input type="name" name="handle_name"><br>
-         メールアドレス　 <input type="email" name="user_mail" ><br>
-         メールアドレス（再確認） <input type="email" name="user_mail_sub"><br>
-         <?php if('user_mail' == 'user_mail_sub'){
-                    echo '<div style="color: red; text-align: center;">'.'上記のメールアドレスと違います。'.'</div>';
-         }   ?>
-         パスワード　　　    <input type="password" name="user_pass" pattern="[0-9A-Za-z]+" minlength="8" maxlength="30"><br>
-         パスワード（再確認）    <input type="password" name="user_mail_sub"><br>
-      </div>
-       <button type="button" class="btn2">新規登録</button>
 </form>
