6bbe645b79af276140337cc8ad1d6f022ca6ac3d
福山拓功 <117243226+FHiraku@users.noreply.github.com>
2023-07-19 09:22:24 +0900

    Delete 福ちゃんねる/04_ソースコード directory

--- 福ちゃんねる/04_ソースコード/loguinkanryou.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="css/loguinkanryou.css">
-<form name="loguinkanryou_form">
-<img src="MicrosoftTeams-image (19).png">
-<div class="roguinkanryou_rogo">
-    <h1>ログイン成功しました。</h1>
-    <h2>次へ</h2>
-
-</div>   
-</form>
